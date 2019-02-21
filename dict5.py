bfd6716afd0761274b0ecf1fb66dce26530e1323
SandyHuang0305 <42807293+SandyHuang0305@users.noreply.github.com>
2019-02-21 21:16:12 +0700

    new file

---
+++ dict5.py
@@ -0,0 +1,14 @@
+x = True
+country_counter = {}
+
+def addone(country):
+	if country in country_counter:
+		country_counter[country] += 1
+	else:
+		country_counter[country] = 1
+
+addone('china')
+addone('japan')
+addone('china')
+
+print(len(country_counter))			
